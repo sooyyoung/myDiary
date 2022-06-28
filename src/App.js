d91743081b9cd8951d3b254e6db3d37cd98e695d
sooyyoung <sooyyyoung@gmail.com>
2022-06-28 22:02:29 +0900

    데이터 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -21,16 +21,24 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   }
 
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) => 
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    )
+  }
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList onDelete={onDelete} diaryList={data}/>
+      <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data}/>
     </div>
   );
 }
--- src/DiaryList.jsx
+++ src/DiaryList.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import DiaryItem from './DiaryItem'
 
-export default function DiaryList({ diaryList, onDelete }) {
+export default function DiaryList({ diaryList, onRemove, onEdit }) {
     return (
         <div className='DiaryList'>
             <h2>일기 리스트</h2>
             <h3>{diaryList.length}개의 일기가 있습니다.</h3>
             <div>
                 {diaryList.map((it) => (
-                    <DiaryItem key={it.id} {...it} onDelete={onDelete}/>
+                    <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit}/>
                 ))}
             </div>
         </div>
