7bbb51d98992b26dbebe550dfef0477b597e5d51
sooyyoung <sooyyyoung@gmail.com>
2022-06-30 00:56:45 +0900

    API 호출, 데이터 초기값 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import './App.css'
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+    
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author : it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random() * 5) + 1,
+        created_date : new Date().getTime(),
+        id : dataId.current++
+      }
+    });
+
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
