8f6939a05da5a5a76849cb995b29b1086740ab8e
sooyyoung <sooyyyoung@gmail.com>
2022-06-30 14:39:16 +0900

    일기 개수 보기 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import './App.css'
@@ -58,9 +58,24 @@ function App() {
     )
   }
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {goodCount, badCount, goodRatio};
+  }, [data.length]);
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
+      <div>전체 일기 개수 : {data.length}</div>
+      <div>기분이 좋은 일기 개수 : {goodCount}</div>
+      <div>기분이 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data}/>
     </div>
   );
