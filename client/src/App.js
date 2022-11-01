5123a76679efcdbbb91a344b3a401467ec345702
sooyoung <sooyyyoung@gmail.com>
2022-11-01 15:39:23 +0900

    API 호출 주석처리

--- client/src/App.js
+++ client/src/App.js
@@ -11,20 +11,20 @@ function App() {
   const dataId = useRef(0);
 
   const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments'
-    ).then((res) => res.json());
+    // const res = await fetch('https://jsonplaceholder.typicode.com/comments'
+    // ).then((res) => res.json());
     
-    const initData = res.slice(0, 20).map((it) => {
-      return {
-        author : it.email,
-        content : it.body,
-        emotion : Math.floor(Math.random() * 5) + 1,
-        created_date : new Date().getTime(),
-        id : dataId.current++
-      }
-    });
+    // const initData = res.slice(0, 20).map((it) => {
+    //   return {
+    //     author : it.email,
+    //     content : it.body,
+    //     emotion : Math.floor(Math.random() * 5) + 1,
+    //     created_date : new Date().getTime(),
+    //     id : dataId.current++
+    //   }
+    // });
 
-    setData(initData);
+    // setData(initData);
   }
 
   useEffect(() => {
